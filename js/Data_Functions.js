175e591aa571d7969f5d6f4ba943c1e6c77ba7aa
Tess <tguefen@gmail.com>
2017-01-17 17:37:55 -0800

    Add files via upload

---
+++ js/Data_Functions.js
@@ -0,0 +1,16 @@
+function Forms_List_Load_Query( filter, sort, offset, count, callback, delegator ) {
+	return AJAX_Call_Module(	callback,
+								'admin',
+								'TGCF',
+								'JSON_Form_Data_Load_Query',
+								'&Form_ID=' + Form_ID + '&Filter=' + EncodeArray( filter ) + '&Sort=' + encodeURIComponent( sort ) + '&Offset=' + encodeURIComponent( offset ) + '&Count=' + encodeURIComponent( count ),
+								delegator );
+}
+function Load_Data_JSON_Columns( callback, delegator ) {
+	return AJAX_Call_Module(	callback,
+								'admin',
+								'TGCF',
+								'Load_Data_JSON_Columns',
+								'&Form_ID=' + Form_ID,
+								delegator );
+}
