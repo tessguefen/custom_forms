9c360fad80da31184c92c184fc1b2b3ecea0c1cc
Tess <tguefen@gmail.com>
2017-10-28 17:10:44 -0700

    ^.^!

--- js/Values_Batchlist.js
+++ js/Values_Batchlist.js
@@ -55,10 +55,10 @@ ValuesBatchlist.prototype.onCreateRootColumnList = function() {
 	var self = this;
 	var columnlist = [];
 	columnlist.push(new MMBatchList_Column_Name( 'Group ID', 'group_id', 'group_id').SetAdvancedSearchEnabled(false).SetDisplayInMenu(false).SetDisplayInList(false) );
+	columnlist.push(new MMBatchList_Column_Name( 'Field ID', 'field_id', 'field_id').SetAdvancedSearchEnabled(false).SetDisplayInMenu(false).SetDisplayInList(false) );
 	for ( i = 0, i_len = self.fields_length; i < i_len; i++ ) {
-		console.log( self.fields[ i ]);
 		if ( self.fields[ i ].type == 'radio' || self.fields[ i ].type == 'select' ) {
-			columnlist.push( new MMBatchList_Column_Text( self.fields[ i ].prompt, 'Fields_' + self.fields[ i ].code, 'Fields:' + self.fields[ i ].code ) );
+			columnlist.push( new Select_Radio_Column( self.fields[ i ] ) );
 		} else if ( self.fields[ i ].type == 'image' ) {
 			columnlist.push( new MMBatchList_Column_Image_Upload( self.fields[ i ].prompt, 'Fields_' + self.fields[ i ].code, 'Fields:' + self.fields[ i ].code ) );
 		} else if ( self.fields[ i ].type == 'checkbox' ) {
@@ -82,7 +82,6 @@ ValuesBatchlist.prototype.onCreate = function() {
 	for ( i = 0, i_len = self.fields_length; i < i_len; i++ ) {
 		record[ 'Fields_' + self.fields[ i ].code ] = '';
 	}
-	console.log(record);
 	return record;
 }
 
@@ -94,4 +93,52 @@ ValuesBatchlist.prototype.onInsert = function( item, callback, delegator ) {
 }
 ValuesBatchlist.prototype.onDelete = function( item, callback, delegator ) {
 	ValuesBatchlist_Function( item.record.mmbatchlist_fieldlist, 'Data_Delete', callback, delegator );
+}
+
+function Select_Radio_Column( column ) {
+	var self = this;
+	MMBatchList_Column_Text.call( this, column.prompt, 'Fields_' + column.code, 'Fields:' + column.code );
+
+	if ( typeof self.retrieveOptions == 'undefined' ) {
+		self.retrieveOptions = [];
+	}
+
+	self.retrieveOptions[ 'Fields_' + column.code ] = column.options;
+
+	self.SetOnDisplayData( this.onDisplayData );
+	self.SetOnDisplayEdit( this.onDisplayEdit );
+}
+DeriveFrom( MMBatchList_Column_Text, Select_Radio_Column );
+
+Select_Radio_Column.prototype.onDisplayEdit = function( record, item ) {
+	var i, i_len;
+	var select;
+	var self = this;
+	var options = self.retrieveOptions[ this.code ];
+
+	select									= newElement( 'select', { 'name': 'type' }, null, null );
+	select.options[ select.options.length ] = new Option( 'Select One', '' );
+
+	for ( i = 0, i_len = options.length; i < i_len; i++ ) {
+		select.options[ select.options.length ] = new Option( options[ i ].prompt, options[ i ].code );
+	}
+
+	for ( i = 0, i_len = select.options.length; i < i_len; i++ )
+	{
+		if ( select.options[ i ].value == record.type )
+		{
+			select.selectedIndex = i;
+			break;
+		}
+	}
+	return select;
+}
+
+Select_Radio_Column.prototype.onDisplayData = function( record ) {
+	var text = newElement( 'div', null, null, null );
+	console.log(record);
+
+	text.innerHTML = '';
+
+	return text;
 }
